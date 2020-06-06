ac5f572a5b2b58ea9df7ac452f1875784226307c
Sudhakar Pai <sudhakar.pai@daimler.com>
2020-06-07 01:36:22 +0530

    Implement insertion sort

---
+++ insertion/insertion_sort.go
@@ -0,0 +1,16 @@
+package insertion
+
+import "github.com/SudhakarPai/sort"
+
+// Sort ... implements the insertion sort on the collection.
+func Sort(slice sort.Collection) {
+	for i := 1; i < slice.Len(); i++ {
+		elmIndex := i
+		for j := i - 1; j >= 0; j-- {
+			if slice.Greater(j, elmIndex) {
+				slice.Swap(j, j+1)
+				elmIndex = j
+			}
+		}
+	}
+}
